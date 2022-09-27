fc7ab0a97ea9b6a996acc655ad0520403da13a7f
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2022-09-27 14:37:31 -0500

    Changes for Bands Finish

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,13 +1,38 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Imagine Dragon'));
+bands.addBand(new Band('Avicii'));
+bands.addBand(new Band('ColdPlay'));
+bands.addBand(new Band('The Chainsmokers'));
+bands.addBand(new Band('Metallica'))
 
 //Sockets Messages
 io.on('connection', client => {
     console.log('Cliente Conectado')
+
+    client.emit('active-bands', bands.getBands());
+    
+
     client.on('disconnect', () => {console.log('Cliente Desconectado')});
 
-    client.on('message', (payload) => {
-        console.log('Message!!!', payload.nombre);
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
 
-        io.emit('message', {admin: 'Nuevo Mensaje'});
-    })
+    client.on('add-band', (payload) => {
+        const newBand = new Band (payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+    
 });
